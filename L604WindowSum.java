291ac818fbd0f4ca9e606a5a4e24f51d42beaa69
Hernan Y.Ke <yuriqiao@gmail.com>
2018-02-23 14:32:09 -0500

    Update and rename LintCode604WindowSum.java to L604WindowSum.java

---
+++ L604WindowSum.java
@@ -0,0 +1,20 @@
+//Hernan Y.Ke
+//02/23/2018
+class L604WindowSum { 
+    public int winSum(int[] nums, int k) {
+        if (nums.length== 0 || nums.length < k || k == 0)
+            return null;
+        ArrayList<Integer> result;
+        int currSum = 0;
+        for (int i = 0; i < k; i++) {
+            currSum += nums[i];
+        }
+        result.add(currSum);
+        for (int i = k; i < nums.size(); i++) {
+            currSum += nums[i];
+            currSum -= nums[i-k];
+            result.add(currSum);
+        }
+        return result.toArray(new Integer[result.size()]);
+    }
+}
